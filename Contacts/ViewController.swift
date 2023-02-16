6a4d7c6ab975c39459253d2194ac8e72ce63b122
bristol <oro.eto.bristol@gmail.com>
2023-02-16 21:06:28 +0200

    create first table

--- Contacts/ViewController.swift
+++ Contacts/ViewController.swift
@@ -13,7 +13,23 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // получаем экземпляр ячейки
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        
+        // конфигурируем ячейку
+        var configuration = cell.defaultContentConfiguration()
+        configuration.text = "Строка \(indexPath.row)"
+        cell.contentConfiguration = configuration
+        
+        // возвращаем сконфигурированный экземпляр ячейки
+        return cell
+    }
+}
