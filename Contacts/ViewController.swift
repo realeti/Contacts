a5c2566520d10fb27106af93df72a5c769213720
bristol <oro.eto.bristol@gmail.com>
2023-02-18 12:21:28 +0200

    added reusable cells

--- Contacts/ViewController.swift
+++ Contacts/ViewController.swift
@@ -21,15 +21,40 @@ extension ViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        // получаем экземпляр ячейки
-        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        var cell: UITableViewCell
         
-        // конфигурируем ячейку
-        var configuration = cell.defaultContentConfiguration()
-        configuration.text = "Строка \(indexPath.row)"
-        cell.contentConfiguration = configuration
+        if let reuseCell = tableView.dequeueReusableCell(withIdentifier: "contactCellIdentifier") {
+            print("Используем старую ячейку для строки с индексом \(indexPath.row)")
+            cell = reuseCell
+        }
+        else {
+            print("Создаем новую ячейку для строки с индексом \(indexPath.row)")
+            cell = UITableViewCell(style: .value1, reuseIdentifier: "contactCellIdentifier")
+        }
         
-        // возвращаем сконфигурированный экземпляр ячейки
+        configuration(cell: &cell, for: indexPath)
         return cell
+    }
+}
+
+extension ViewController {
+    private func configuration(cell: inout UITableViewCell, for indexPath: IndexPath) {
+        if #available(iOS 14, *) {
+            var configuration = cell.defaultContentConfiguration()
+            configuration.text = "Строка #\(indexPath.row)"
+            
+            if indexPath.row == 0 {
+                configuration.secondaryText = "secondaryText"
+            }
+            
+            cell.contentConfiguration = configuration
+        }
+        else {
+            cell.textLabel?.text = "Cтрока \(indexPath.row)"
+            
+            if indexPath.row == 0 {
+                cell.detailTextLabel?.text = "secondaryText"
+            }
+        }
     }
 }
